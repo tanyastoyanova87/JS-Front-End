a8486235d1aae571e6d99abc9c1b5a7c88cd8ff6
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2024-07-09 15:30:44 +0300

    changed file

--- JS-Front-End/l3-functions-and-statements-lab/01-format-grade.js
+++ JS-Front-End/l3-functions-and-statements-lab/01-format-grade.js
@@ -1,18 +1,18 @@
 function formatGrade(grade) {
-  let result = '';
-  if(grade < 3) {
-    result = 'Fail';
-  } else if(grade < 3.5) {
-    result = 'Poor';
-  } else if(grade < 4.5) {
-    result = 'Good';
-  } else if(grade < 5.5) {
-    result = 'Very good';
-  } else {
-    result = 'Excellent';
-  }
+    let result = '';
+    if (grade < 3) {
+        result = 'Fail';
+    } else if (grade < 3.5) {
+        result = 'Poor';
+    } else if (grade < 4.5) {
+        result = 'Good';
+    } else if (grade < 5.5) {
+        result = 'Very good';
+    } else {
+        result = 'Excellent';
+    }
 
-  console.log(`${result} (${grade < 3 ? 2 : grade.toFixed(2)})`);
+    console.log(`${result} (${grade < 3 ? 2 : grade.toFixed(2)})`);
 }
 
 formatGrade(2.99);
