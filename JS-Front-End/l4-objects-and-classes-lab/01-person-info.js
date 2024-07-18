2b9fa3f2be8f46ddb47323ccd499fc6e9ef310de
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2024-07-18 14:17:50 +0300

    Added new exercise

---
+++ JS-Front-End/l4-objects-and-classes-lab/01-person-info.js
@@ -0,0 +1,14 @@
+function solve(firstName, lastName, age) {
+    const person = {
+        firstName,
+        lastName,
+        age,
+    }
+
+    return person;
+}
+
+solve("Peter",
+    "Pan",
+    "20"
+)
